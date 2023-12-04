263e8c2391f447534870852218777883cfb5f901
teja1000 <99501668+teja1000@users.noreply.github.com>
2023-12-04 12:19:55 +0000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist', // Specify the output directory for the build
-  },
 });
 
