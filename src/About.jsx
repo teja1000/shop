fdae41b65f08ecf085f38f3ade985acc71264130
teja1000 <99501668+teja1000@users.noreply.github.com>
2023-12-04 12:03:15 +0000

    Update 5 files

---
+++ src/About.jsx
@@ -0,0 +1,12 @@
+export default About=()=>{
+    return (
+        <div>
+            <h1>This Water plant belons to Ganga Jal trust founded by Ananda Rao Thella  </h1>
+            <h2>it was started on 4th june 2020 </h2>
+            <p>Not only sells water but also Eggs and cool drinks</p>
+
+
+            </div>
+
+    );
+}
--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Card from './card';
 import data from './data';
 
+
+
 export default function App() {
+  const[sign,setsign]=useState("login");
+  const handler=()=>{
+    setsign((prevText) => (prevText === 'login' ? 'logout' : 'login'));
+  }
   return (
     <div>
       <h1 className="title">Gangajal water Plant</h1>
@@ -17,14 +23,17 @@ export default function App() {
           <li>
             <button>Cool drinks</button>
           </li>
+          <li>About</li>
+          <li><button className='btn' onClick={handler}>{sign=== 'login' ? 'Login' : 'Logout'}</button></li>
         </ul>
-      </div>
-
-      <div className='body'>
-      {data.map((item) => (
-        <Card key={item.one.number} props={item} />
-      ))}
-      </div>
+        
+        </div>
+        <div className='body'>
+        {data.map((item) => (
+          <Card key={item.one.number} props={item} />
+        ))}
+        </div>
+       
     </div>
   );
 }
